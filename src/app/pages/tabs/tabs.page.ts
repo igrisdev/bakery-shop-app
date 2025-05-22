967f15c45c7c58f247ae5757b773a26c03bf1ec5
igrisdev <johanalvarezz237@gmail.com>
2025-05-21 23:04:31 -0500

    refactor: restructure app routes and implement tabs navigation; remove unused home page components

---
+++ src/app/pages/tabs/tabs.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {
+  IonIcon,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonLabel,
+} from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import { chatboxEllipses, heart, home, location, person } from 'ionicons/icons';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.page.html',
+  styleUrls: ['./tabs.page.scss'],
+  imports: [IonIcon, IonTabBar, IonTabButton, IonTabs, IonLabel],
+})
+export class TabsPage {
+  constructor() {
+    addIcons({ home, location, heart, chatboxEllipses, person });
+  }
+}
